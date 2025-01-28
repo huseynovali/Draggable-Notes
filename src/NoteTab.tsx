b72222f21278e2b732284dbc9c699aaea089397a
User <huseynovali612@gmail.com>
2025-01-28 21:17:55 +0400

    dragable

--- src/NoteTab.tsx
+++ src/NoteTab.tsx
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function NoteTab() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [offset, setOffset] = useState({ x: 0, y: 0 });
+  const [position, setPosition] = useState({ x: 100, y: 100 });
 
- const dragContainer = (e: React.DragEvent<HTMLDivElement>) => {
-    console.log('e', e)
-    
-  }
+  const dragStartContainer = (e) => {
+    setIsDragging(true);
+    setOffset({
+      x: e.clientX - position.x,
+      y: e.clientY - position.y,
+    });
+  };
 
+  const dragContainer = (e) => {
+    if (isDragging) {
+      const newX = e.clientX - offset.x;
+      const newY = e.clientY - offset.y;
+      setPosition({ x: newX, y: newY });
+    }
+  };
 
+  const dragEndContainer = () => {
+    setIsDragging(false);
+  };
 
   return (
-    <div className='note-tab' draggable='true' onDrag={(e)=>dragContainer(e)} >
-
+    <div
+      className="note-tab"
+      style={{
+        position: "absolute",
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+        width: "100px",
+        height: "100px",
+        backgroundColor: "lightcoral",
+        cursor: isDragging ? "grabbing" : "grab",
+      }}
+      onMouseDown={dragStartContainer}
+      onMouseMove={dragContainer}
+      onMouseUp={dragEndContainer}
+      onMouseLeave={dragEndContainer}
+    >
+      Drag Me
     </div>
-  )
+  );
 }
 
-export default NoteTab
+export default NoteTab;
