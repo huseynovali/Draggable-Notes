a45024ed6ccb4b09810d852599a3523bfb331e47
User <huseynovali612@gmail.com>
2025-01-28 22:33:08 +0400

    config design

--- src/NoteTab.tsx
+++ src/NoteTab.tsx
@@ -29,20 +29,30 @@ function NoteTab() {
     <div
       className="note-tab"
       style={{
-        position: "absolute",
         left: `${position.x}px`,
         top: `${position.y}px`,
-        width: "100px",
-        height: "100px",
-        backgroundColor: "lightcoral",
         cursor: isDragging ? "grabbing" : "grab",
       }}
-      onMouseDown={dragStartContainer}
-      onMouseMove={dragContainer}
-      onMouseUp={dragEndContainer}
-      onMouseLeave={dragEndContainer}
     >
-      Drag Me
+      <div
+        className="note_tab_header"
+        onMouseDown={dragStartContainer}
+        onMouseMove={dragContainer}
+        onMouseUp={dragEndContainer}
+        onMouseLeave={dragEndContainer}
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 448 512"
+          height={20}
+          width={20}
+        >
+          <path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm32-192l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48zm32-192l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40l48 0c22.1 0 40-17.9 40-40l0-48z" />
+        </svg>
+      </div>
+      <div className="note_body " draggable="false">
+        <textarea name="" id="" className="note_textarea"></textarea>
+      </div>
     </div>
   );
 }
